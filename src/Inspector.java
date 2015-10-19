26f8dc8ae01edcd1f16664f9c9f0e263e28ecf66
Andrew <pext99@gmail.com>
2015-10-19 00:50:58 -0600

    added interfaces and parameter types
    The Inspector class can now detect interfaces and the parameter types.

--- src/Inspector.java
+++ src/Inspector.java
@@ -4,22 +4,66 @@
  * 10045041
  * References:
  * To print all elements in an array line by line (for readability)
- * http://stackoverflow.com/questions/409784/whats-the-simplest-way-to-print-a-java-array
  */
+import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 import java.util.Arrays;
 
 
 public class Inspector {
 	public void inspect(Object obj, boolean recursive){
 		//get declaring class
-		Class declaringClass = obj.getClass();
+		Class declaringClass = obj.getClass(); //class object
 		System.out.println("\nDeclaring class: " + declaringClass.getName() + "\n");
 		
-		//stores all methods in an array
+		Class[] interfaces = declaringClass.getInterfaces();
+		int interfaceLength = interfaces.length;
+		for(int i = 0; i < interfaceLength; i++){
+			System.out.println("Interface: " + interfaces[i].getSimpleName());
+		}
+		System.out.println();//newline
+		
+		//stores all methods in an array		
 		Method methodsArray[] = declaringClass.getMethods(); 
+		int methodsArrayLength = methodsArray.length;
 		System.out.print("Methods in Class: \n");
-		Arrays.asList(methodsArray).stream().forEach(s -> System.out.println(s)); //to print all elements of an array line by line 
+		for(int i = 0; i < methodsArrayLength; i++){
+			System.out.println(methodsArray[i]);
+			
+			//return parameter types
+			Class[] parameterTypes = methodsArray[i].getParameterTypes();
+			int parameterLength = parameterTypes.length;
+			for(int j = 0; j < parameterLength; j++){
+				System.out.println("\tParameter Type: " + parameterTypes[j].toString());
+			}
+		}
+		
+		//http://stackoverflow.com/questions/409784/whats-the-simplest-way-to-print-a-java-array
+		//Arrays.asList(methodsArray).stream().forEach(s -> System.out.println(s + "\n\t")); //to print all elements of an array line by line
+		
+		
+		Class superClass = declaringClass.getSuperclass();
+		while (superClass != null && superClass.getName() != "java.lang.Object"){//traverse through hierarchy
+			System.out.println("Superclass: " + superClass.getName());
+			superClass = superClass.getSuperclass();
+		}
+		
+		//refactoring for getSuperclass
+		/*Class subClass = null;
+		while(superClass != null){
+			String className = superClass.getName();
+			System.out.println("\nSuperclass: is the Subclass of " + (className));
+			subClass = superClass;
+			superClass = subClass.getSuperclass(); 
+		}*/
+		
+		//System.out.println("\nSuperclass: " + superClass);
+		
+		
+		
+		
+		
 		//System.out.println("Methods in Class: " + Arrays.toString(methodsArray));
 		
 		//get all methods within the class
