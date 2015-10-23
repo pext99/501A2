30821055e314c1dbc1797ab2a081ee41c497755d
Andrew <pext99@gmail.com>
2015-10-23 00:57:48 -0600

    All fields + recursive
    fields are completed and so is the recursive inspection.

--- src/Inspector.java
+++ src/Inspector.java
@@ -7,35 +7,37 @@
  */
 import java.lang.reflect.*;
 import java.util.Arrays;
+import java.util.*;
 
 
 public class Inspector {
 	public void inspect(Object obj, boolean recursive){
 		Class classObj = obj.getClass();
+		Vector read = new Vector();
 		
 		//print/get declaring class
 		Class declaringClass = obj.getClass(); //class object
-		System.out.println("\nDeclaring class: " + declaringClass.getName() + "\n");
+		System.out.println("\nCLASS: " + declaringClass.getName() + "\n");
 		Class superClass = declaringClass.getSuperclass();
 		while (superClass != null && superClass.getName() != "java.lang.Object"){//traverse through hierarchy
-			System.out.println("Superclass: " + superClass.getName());
+			System.out.println("SUPERCLASS: " + superClass.getName());
 			superClass = superClass.getSuperclass();
 		}
 		//print interfaces
 		Class[] interfaces = declaringClass.getInterfaces();
 		int interfaceLength = interfaces.length;
 		for(int i = 0; i < interfaceLength; i++){
-			System.out.println("Interface: " + interfaces[i].getSimpleName());
+			System.out.println("INTERFACE: " + interfaces[i].getSimpleName());
 		}
 		System.out.println();//newline
 		
 		//stores all methods in an array		
 		Method methodsArray[] = declaringClass.getMethods(); 
 		int methodsArrayLength = methodsArray.length;
-		System.out.print("Methods in Class: \n");
+		//System.out.print("Methods in Class: \n");
 		//name of method
 		for(int i = 0; i < methodsArrayLength; i++){
-			System.out.println(methodsArray[i]);
+			System.out.println("METHOD: " + methodsArray[i]);
 			//return parameter types
 			Class[] parameterTypes = methodsArray[i].getParameterTypes();
 			int parameterLength = parameterTypes.length;
@@ -72,65 +74,34 @@ public class Inspector {
 			String modifierTypes = Modifier.toString(constructorsArray[i].getModifiers());
 			System.out.println("\tModifier Types: " + modifierTypes);
 		}
-		
-		//get fields
-		Field fieldArray[] = declaringClass.getDeclaredFields();
-		//prints name, modifier, type
-		for(int i = 0; i < fieldArray.length; i++){
-			System.out.println("FIELD: " + fieldArray[i].getName());
-			String getModifier = Modifier.toString(fieldArray[i].getModifiers());
-			Object getType = fieldArray[i].getType();
-			System.out.println("\tType: " + getType);
-			System.out.println("\tModifier: " + getModifier);
-			
-		}
-		
-		/*//print field name
-			if (fieldArray[i].getType().isPrimitive()){
-				System.out.println("FIELD (primitive): " + fieldArray[i].getName());
-			}
-			else if(fieldArray[i].getType().isArray()){
-				try {
-					fieldArray[i].setAccessible(true);
-					for(int j = 0; j < Array.getLength(fieldArray[j].get(obj)); j++){
-						System.out.println("asdf" + Array.get(obj, j));
-					}
-				} catch (IllegalArgumentException | IllegalAccessException e) {
-					e.printStackTrace();
+		System.out.println();
+		readFields(obj, classObj, read, recursive);
+	}
+	private void readFields(Object obj, Class classObj, Vector read, boolean recursive){
+				//get fields
+				Field fieldArray[] = classObj.getDeclaredFields();
+				//prints name, modifier, type
+				for(int i = 0; i < fieldArray.length; i++){
+					System.out.println("FIELD: " + fieldArray[i].getName());
+					
+					Object getType = fieldArray[i].getType();
+					String getModifier = Modifier.toString(fieldArray[i].getModifiers());
+
+					System.out.println("\tType: " + getType);
+					System.out.println("\tModifier: " + getModifier);
 				}
-			}else{
-				System.out.println("not primitive");
-			}*/
-		
-		//refactoring for getSuperclass
-		/*Class subClass = null;
-		while(superClass != null){
-			String className = superClass.getName();
-			System.out.println("\nSuperclass: is the Subclass of " + (className));
-			subClass = superClass;
-			superClass = subClass.getSuperclass(); 
-		}*/
-		
-		//System.out.println("\nSuperclass: " + superClass);
-		
-		
-		
-		
-		
-		//System.out.println("Methods in Class: " + Arrays.toString(methodsArray));
-		
-		//get all methods within the class
-		/*Class methodsInClass = ClassA.class;
-		Method methodsArray[] = methodsInClass.getMethods();
-		System.out.println(Arrays.toString(methodsArray));
-		*/
-		
-		/*Object myObj = new Object();
-		Class classObject = myObj.getClass();
-		System.out.println(classObject);*/
-		
-		/*Class reflectClass1 = ClassB.class;
-		String className1 = reflectClass1.getName();
-		System.out.println(className1);*/
+				//recursive inspection
+				for(int i = 0; i < fieldArray.length; i++){
+					Field field = classObj.getDeclaredFields()[i];
+					if(!fieldArray[i].getType().isPrimitive()){
+						field.setAccessible(true);
+						try{
+							System.out.println("Hash code from: " + fieldArray[i].getName() + " - " + obj.hashCode());
+							System.out.println("Value from: " + fieldArray[i].getName() + " - " + field.get(obj));
+						}catch(IllegalArgumentException | IllegalAccessException e){
+							e.printStackTrace();
+						}
+					}
+				}
 	}
 }
